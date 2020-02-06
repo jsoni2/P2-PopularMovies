2dfaafb61f9bce5eb08723b016e4d276045ca874
sonij02 <sonij02@thryv.com>
2020-02-06 14:18:38 -0600

    Add AsyncTask

--- app/src/main/java/com/janak/popularmovies/MainActivity.java
+++ app/src/main/java/com/janak/popularmovies/MainActivity.java
@@ -2,6 +2,7 @@ package com.janak.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // AsyncTask
+    public class FetchMovies extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
     }
 }
