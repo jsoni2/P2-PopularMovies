51eae1eb0528de61334051145c6290536a42c51b
sonij02 <sonij02@thryv.com>
2020-02-06 14:37:49 -0600

    Update MainActivity.java

--- app/src/main/java/com/janak/popularmovies/MainActivity.java
+++ app/src/main/java/com/janak/popularmovies/MainActivity.java
@@ -2,10 +2,13 @@ package com.janak.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Movie;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.pb_loading)
     ProgressBar mProgressBar;
+
+    String popularMoviesURL;
+    String topRatedMoviesURL;
+
+    String YOUR_API_KEY = "67abda4d931f7a093afa16b89a87dcf9";
+
+    ArrayList<Movie> mPopularList;
+    ArrayList<Movie> mTopTopRatedList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... voids) {
+            popularMoviesURL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="+YOUR_API_KEY;
+
+            topRatedMoviesURL = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key="+YOUR_API_KEY;
+
+            mPopularList = new ArrayList<>();
+            mTopTopRatedList = new ArrayList<>();
+            
             return null;
         }
 
