2bf2301ee66715cb27886b596174f39dcbc5b58d
sonij02 <sonij02@thryv.com>
2020-02-10 13:59:11 -0600

    Send pojo in Intent

--- app/src/main/java/com/janak/popularmovies/DetailActivity.java
+++ app/src/main/java/com/janak/popularmovies/DetailActivity.java
@@ -2,7 +2,11 @@ package com.janak.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.janak.popularmovies.model.Movie;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intentThatStartedActivity = getIntent();
+
+        if (intentThatStartedActivity != null) {
+            if (intentThatStartedActivity.hasExtra("Movie")) {
+                Movie detailMovie = (Movie) intentThatStartedActivity.getSerializableExtra("Movie");
+                Toast.makeText(this, detailMovie.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
--- app/src/main/java/com/janak/popularmovies/MainActivity.java
+++ app/src/main/java/com/janak/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.janak.popularmovies;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -48,7 +49,11 @@ public class MainActivity extends AppCompatActivity {
         new FetchMovies().execute();
 
         mGridView.setOnItemClickListener((adapterView, view, i, l) -> {
-            Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            Movie clickedMovie = (Movie) adapterView.getItemAtPosition(i);
+            Class destinationClass = DetailActivity.class;
+            Intent intent = new Intent(MainActivity.this, destinationClass);
+            intent.putExtra("Movie", clickedMovie);
+            startActivity(intent);
         });
     }
 
